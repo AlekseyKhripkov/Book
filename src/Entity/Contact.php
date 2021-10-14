b63c344df47e8d9d14e9a68a14ffefc5aea8f54f
Aleksey Khripkov <san4o3540479@icloud.com>
2021-10-14 19:32:49 +0300

    Book

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -5,4 +5,133 @@ namespace App\Entity;
 class Contact
 {
 
+    protected $ID;
+    protected $name;
+    protected $company;
+    protected $telephone;
+    protected $email;
+    protected $dateOfBirth;
+    protected $photoUser;
+
+    /**
+     * @return mixed
+     */
+    public function getID()
+    {
+        return $this->ID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     * @return Contact
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    /**
+     * @param mixed $company
+     * @return Contact
+     */
+    public function setCompany($company)
+    {
+        $this->company = $company;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTelephone()
+    {
+        return $this->telephone;
+    }
+
+    /**
+     * @param mixed $telephone
+     * @return Contact
+     */
+    public function setTelephone($telephone)
+    {
+        $this->telephone = $telephone;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateOfBirth()
+    {
+        return $this->dateOfBirth;
+    }
+
+    /**
+     * @param mixed $dateOfBirth
+     * @return Contact
+     */
+    public function setDateOfBirth($dateOfBirth)
+    {
+        $this->dateOfBirth = $dateOfBirth;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhotoUser()
+    {
+        return $this->photoUser;
+    }
+
+    /**
+     * @param mixed $photoUser
+     * @return Contact
+     */
+    public function setPhotoUser($photoUser)
+    {
+        $this->photoUser = $photoUser;
+        return $this;
+    }
+
+
+
+
+
+
 }
